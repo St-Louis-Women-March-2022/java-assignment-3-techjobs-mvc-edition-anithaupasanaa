55d5edda2475b14dcabfa9259b3412e11c6716b8
anithaupasanaa <anithaupasanaa@gmail.com>
2022-11-16 22:07:05 -0600

    Final Commit

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -5,6 +5,7 @@ import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.io.*;
@@ -32,15 +33,28 @@ public class SearchController {
 
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @RequestMapping(value = "results")
+    @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        model.addAttribute("type", searchType);
+       // jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+       // model.addAttribute("type", searchType);
+       // model.addAttribute("columns", columnChoices);
+       //model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
+     //   model.addAttribute("jobs", jobs);
+       // return "search.html";
+        if (searchTerm.equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs =  JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+
         model.addAttribute("columns", columnChoices);
-        model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
-        model.addAttribute("jobs", jobs);
-        return "search.html";
+        model.addAttribute("jobs",jobs);
+
+        return "search";
+        }
+
     }
-}
+
 
